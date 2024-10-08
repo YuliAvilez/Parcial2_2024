0d6f11acfcb6b8cf879f46852270011c02420c23
YULI AVILEZ GAZCON <yavilez09@gmail.com>
2024-10-08 05:03:56 -0500

    Se empieza a construir la lógica del código, para validar el número de jugadores

--- Punto1_Juego/Punto1_Juego/Program.cs
+++ Punto1_Juego/Punto1_Juego/Program.cs
@@ -3,6 +3,21 @@
     static void Main()
     {
         // Asignación de variables
-        int NumJugadores, RangoMax = 50;
+        int NumJugadores, RangoMax = 50, NumObjetivo;
+        bool JuegoGanado = false;
+
+        Console.Write(" Ingresa el número de jugadores (2-4): ");
+        NumJugadores = int.Parse(Console.ReadLine());
+
+        if (NumJugadores == 3)
+        {
+            RangoMax = 100;
+        }
+        else if (NumJugadores == 4)
+        {
+            RangoMax = 200;
+        }
+            Random random = new Random();
+        NumObjetivo = random.Next(0,RangoMax + 1);
     }
 }
